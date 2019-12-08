863404d9326579fa6c0ee2adf71185c61164e7f5
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2019-12-08 19:02:56 -0300

    cambios

--- U03_Pilas/Ej-05/main.cpp
+++ U03_Pilas/Ej-05/main.cpp
@@ -2,6 +2,36 @@
 #include "../Pila/Pila.h"
 
 int main() {
-    std::cout << "Ejercicio 03/05\n" << std::endl;
-    return 0;
+    char arr[10];
+    Pila<char> P;
+
+    std::cout << "Ingrese palabra" << std::endl;
+    std::cin >> arr;
+    for (int i = 0; arr[i] != '\0'; i++) {
+        if (arr[i] >= 'a' && arr[i] <= 'z') {
+            std::cout << arr[i];
+        }
+        if (arr[i] == ')') {
+            while (P.peek() != '(') {
+                std::cout << P.pop();
+            }
+            P.pop();       
+        }
+        try {
+            if ((arr[i] == '+' || arr[i] == '-') && (P.peek() == '+' || P.peek() == '-')) {
+                std::cout << P.pop();
+                P.push(arr[i]);
+            }
+            if ((arr[i] == '*' || arr[i] == '/') && (P.peek() == '*' || P.peek() == '/')) {
+                std::cout << P.pop();
+                P.push(arr[i]);
+            }
+        } catch (int e) {
+        }
+        if (arr[i] == '(' || arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/' || arr[i] == '^')
+            P.push(arr[i]);
+    }
+    while (!P.esVacia())
+        std::cout << P.pop();
+
 }

0dabbeeedba60ae0853146a62de1742bbd8342d4
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2019-12-08 19:05:03 -0300

    Listo

--- U05_Arbol/Ej-01/main.cpp
+++ U05_Arbol/Ej-01/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../Arbol/ArbolBinarioAVL.h"
+using namespace std;
 int main() {
     std::cout << "Ejercicio 05/01\n" << std::endl;
-    return 0;
+
+    ArbolBinarioAVL<int> A;
+    int numeros[31];
+
+    int i, j;
+
+    srand(time(nullptr));
+    for (i=0; i<31; i++)
+    {
+        numeros[i]= (rand() % 400) + 100;
+        A.put(numeros[i]);
+    }
+
+    cout<<"Numeros del arreglo:";
+    for (i=0; i<30; i++)
+    {
+        cout<<numeros[i]<<endl;
+    }
+
+    cout<<"--------------------------";
+    A.print();
+
 }

fdf992dc3c84eb1b20ae68e4034ac73fe094df0b
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2019-12-08 19:06:32 -0300

    Listo

--- U05_Arbol/Ej-02/main.cpp
+++ U05_Arbol/Ej-02/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-
+#include "../Arbol/ArbolBinarioAVL.h"
+using namespace std;
 int main() {
     std::cout << "Ejercicio 05/02\n" << std::endl;
-    return 0;
+
+    ArbolBinarioAVL<int> A;
+
+    int n,dato;
+    cout<<"ingrese cantidad de elementos:";
+    cin>>n;
+    cout<<"ingrese elementos del arbol";
+    for (int i=0; i<n; i++)
+    {
+        cin>>dato;
+        A.put(dato);
+    }
+    cout<<"INORDER:"<<endl;
+    A.inorder();
+    cout<<"PREORDER:"<<endl;
+    A.preorder();
+    cout<<"POSTORDER:"<<endl;
+    A.posorder();
 }

8c62ba3d247f6f0014bf1aed7d6119908d98f866
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2019-12-08 20:07:47 -0300

    Terminado

--- U05_Arbol/Ej-04/main.cpp
+++ U05_Arbol/Ej-04/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "../Arbol/ArbolBinarioAVL.h"
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 05/03\n" << std::endl;
-    return 0;
+    std::cout << "Ejercicio 05/02\n" << std::endl;
+    int n;
+    ArbolBinarioAVL<int> A;
+
+    for (int i=0; i<5; i++)
+    {std::cin>>n;
+     A.put(n);}
+
+    A.print();
+
+    cout<<endl;
+   A.ImagenEspecular();
+
+   A.print();
+
 }

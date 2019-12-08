08ff3db49cd4f44df7d7597ccdf72bbf5caad1f1
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2019-12-08 19:07:28 -0300

    Listo

--- U05_Arbol/Ej-03/main.cpp
+++ U05_Arbol/Ej-03/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-
+#include "../Arbol/ArbolBinarioAVL.h"
+using namespace std;
 int main() {
     std::cout << "Ejercicio 05/03\n" << std::endl;
-    return 0;
+
+    ArbolBinarioAVL<int> A;
+
+    int n,dato;
+    cout<<"ingrese cantidad de elementos:";
+    cin>>n;
+    cout<<"ingrese elementos del arbol";
+    for (int i=0; i<n; i++)
+    {
+        cin>>dato;
+        A.put(dato);
+    }
+    A.print();
+    cout<<A.SumarHasta();
+
+
+
+
 }
